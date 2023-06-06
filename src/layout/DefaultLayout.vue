f7eead4b26317f8f5d24a36c5c16404bef295c72
Ayagoumi <agoumi94@gmail.com>
2023-06-06 16:38:23 +0100

    fix: spelling in the footer

--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -15,10 +15,10 @@
                         />
                         <img v-else src="@/assets/DarkModeLogo.svg" class="logo" />
                         <span class="desc">
-                            Camino is a fast, high-through put open-source consortium blockchain
-                            enabling the creation of travel related products. Camino’s tech stack
-                            and consensus protocol deliver unmatched speed, security and reliability
-                            within the network
+                            Camino is a fast, high-throughput open-source consortium blockchain
+                            enabling the creation of travel-related products. Camino’s tech stack
+                            and consensus protocol deliver unmatched speed, security, and
+                            reliability within the network.
                         </span>
                     </div>
                     <div class="second-section">
