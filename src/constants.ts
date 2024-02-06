f414742797d419b9bd60f4272230d38930ab0737
Ayagoumi <agoumi94@gmail.com>
2024-02-06 17:44:18 +0100

    Add AddressState enum and isValidPChainAddress helper function

--- src/constants.ts
+++ src/constants.ts
@@ -9,3 +9,14 @@ export enum KYC_VARIANT {
     KYC_BASIC = 'kyc_basic',
     KYB_BASIC = 'kyb_basic',
 }
+
+export enum AddressState {
+    ROLE_ADMIN = 0,
+    ROLE_KYC = 1,
+    ROLE_OFFERS_ADMIN = 2,
+    KYC_VERIFIED = 32,
+    KYC_EXPIRED = 33,
+    CONSORTIUM = 38,
+    NODE_DEFERRED = 39,
+    OFFERS_CREATOR = 50,
+}
